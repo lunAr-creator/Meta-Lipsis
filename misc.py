878720ddc435958b142bb92f7bf35b64ace6ae38
lunAr-creator <lunArcreator3@gmail.com>
2022-01-31 23:18:45 +0000

    new commit after revert of failed force push

--- misc.py
+++ misc.py
@@ -1,20 +1,18 @@
 def get_help(given_list):
-    print(r"""
- Help:
- -----------------------
-        """)
+    print(" Help: \n -----------------------")
+    
     for i in range(len(given_list)):
         output2 = []
+
         for j in range(len(given_list[i])):
             output = f"{given_list[i][j]}"
             output2.append(output)
+
         print(f" {': '.join(output2)}")
+
     print(" -----------------------")
 
 def get_input(string: str, valid_options: list) -> str:
-    """
-    Deals with error checking for inputs
-    """
     while True:
         user_input = input(string)
         hmm = user_input[0].upper() + user_input[1:len(user_input)]
@@ -22,7 +20,4 @@ def get_input(string: str, valid_options: list) -> str:
         if user_input in valid_options or hmm in valid_options:
             return hmm
 
-        # if user_input in valid_options:
-        #     return user_input
-        
         print(f" -Please select from: {' or '.join(valid_options)}-")

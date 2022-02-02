82aedde82633e6644f6bd4772782133c9f51fee7
lunAr-creator <lunArcreator3@gmail.com>
2022-02-02 19:04:34 +0000

    updated main.py authorisation and get_input funcs

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import os
 import sys
 
 from visualUtils import moving_ellipsis, print_text, menu_art, spinner, screen_line, line_by_line
-from misc import get_input, get_help
 
 with open(os.devnull, 'w') as f:
     oldstdout = sys.stdout
@@ -19,6 +18,41 @@ with open(os.devnull, 'w') as f:
 
     sys.stdout = oldstdout
 
+
+#Miscellaneous Functions
+def get_help(given_list):
+    print(" Help: \n -----------------------")
+    
+    for i in range(len(given_list)):
+        output2 = []
+
+        for j in range(len(given_list[i])):
+            output = f"{given_list[i][j]}"
+            output2.append(output)
+
+        print(f" {': '.join(output2)}")
+
+    print(" -----------------------")
+
+def get_input(string: str, valid_options: list) -> str:
+    while True:
+        user_input = input(string)
+        capitalized = user_input[0].upper() + user_input[1:len(user_input)]
+
+        if user_input in valid_options or capitalized in valid_options:
+            return capitalized
+
+        elif capitalized == 'Exit':
+        	print("\n Ending Program")
+        	return False
+
+        print(f" -Please select from: {' or '.join(valid_options)}-")
+
+def generate_token(size=15):
+    return secrets.token_urlsafe(size)[:size]
+
+
+#Make hashes for comparisons and storing in .txt file 'accountfile.txt'
 def make_hash(data):
 	return hashlib.sha256(str.encode(data)).hexdigest()
 
@@ -28,10 +62,8 @@ def check_hash(data, hash):
 
 	return False
 
-#generates token for securely connecting to server
-def generate_token(size=15):
-    return secrets.token_urlsafe(size)[:size]
 
+#Functions that deal with creation, authorisation and login process of the user
 def get_existing_users():
     with open("accountfile.txt", "r") as fp:
          for line in fp.readlines():
@@ -39,7 +71,6 @@ def get_existing_users():
              yield username, password
 
 def is_authorized(username, password):
-    # return any((user == (username, password) for user in get_existing_users()))
     return any((check_hash(username, user[0])) and check_hash(password, user[1]) for user in get_existing_users())
 
 def get_user():
@@ -88,20 +119,23 @@ def make_user():
 		return False
 
 def authorisation():
-	username, password = get_user()
+	if len(list(get_existing_users())) == 0:
+		print(" No exisiting users in account file")
 
-	if is_authorized(username, password):
-		print(f"\n Welcome back {username}")
+		moving_ellipsis("\n Redirecting to user creation process")
+		make_user()
 
-	elif not is_authorized(username, password):
-		while not is_authorized(username, password):
-			if is_authorized(username, password):
-				print(f"\n Welcome back {username}")
-				break
+	else:
+		username, password = get_user()
 
+		if is_authorized(username, password):
+			print(f"\n Welcome back {username}")
+
+		else:
 			print("\n Incorrect login details")
-			username, password = get_user()
 
+
+#Main menu loop!!!
 def main():
 	# menu_art(2)
 	line_by_line()
@@ -123,5 +157,11 @@ def main():
 		elif choice == "Help":
 			get_help([["Login", "Login??"], ['New User', 'Create new user']])
 
+		elif choice == False:
+			break
+
+
 if __name__ == '__main__':
 	main()
+
+
